8d14354107ce477ab3a6fb9c5fec42803b578e82
Mohammad <mim.seyedi@gmail.com>
2025-01-21 19:48:50 +0330

    The DefaultValidationError error was removed and exception descriptions were written.

--- src/argsv/errors.py
+++ src/argsv/errors.py
@@ -1,35 +1,37 @@
 """
+@rgs✔
 
+This file is related to custom argsv errors and exceptions.
+This file implements the exceptions that are used and raised
+during the validation process.
 """
 
 
 class ArgsVError(Exception):
     """
-
+    This exception is the most basic exception from
+    which all other exceptions inherit.
     """
 
 
 class ValidationError(ArgsVError):
     """
-
-    """
-
-
-class DefaultValidationError(ValidationError):
-    """
-
+    This exception occurs when the validation process fails and stops.
     """
 
 
 class ValidatorError(ArgsVError):
     """
-
+    This exception is related to validators.
+    When validators generate their own specific error.
     """
 
 
 class PatternError(ArgsVError):
     """
-
+    This exception is related to validation patterns.
+    This exception is raised when a pattern does not have
+    its correct structure for any reason.
     """
 
 
